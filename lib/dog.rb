be557962d54e21874da16ac0be0374a04141f135
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-02-23 22:34:06 +0300

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,20 @@
-class Dog
-end
+class Dog < ActiveRecord::Base
+    # attr_accessor :name, :breed
+  
+    # def self.create(name:, breed:)
+    #   dog = Dog.new(name: name, breed: breed)
+    #   dog.save
+    #   dog
+    # end
+  
+    # def self.save
+    #       self.id = self.class.last.id
+    # end
+    # def self.update
+    #  dog=self.update(name: self.name, breed: self.breed)
+    #  dog.save
+    # end
+
+  
+  end
+  
